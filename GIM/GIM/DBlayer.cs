dad6b62c4d617de59ce073217f7eb2b3980291fc
Kamil Nematli <kamil.nematli@gmail.com>
2017-03-01 16:47:06 +0400

    First commit

---
+++ GIM/GIM/DBlayer.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GIM
+{
+    class DBlayer
+    {
+        string connectionString = "server=pmsasql01.database.windows.net;database=GamesModelDB02;uid=SQLAdmin01;pwd=UD6JvqIY9iLlX57PT3U1";
+
+        public bool IsServerConnected()
+        {
+            using (var l_oConnection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    l_oConnection.Open();
+                    return true;
+                }
+                catch (SqlException)
+                {
+                    return false;
+                }
+            }
+        }
+
+        #region Get data
+
+        public DataSet GetIssues()
+        {
+            string _sql = "";
+            DataSet ds = new DataSet();
+
+            _sql = "";
+
+            SqlConnection conn = new SqlConnection(@connectionString);
+            conn.Open();
+            SqlDataAdapter adapter = new SqlDataAdapter(_sql, conn);
+            adapter.Fill(ds);
+            conn.Close();
+            return ds;
+        }
+
+        #endregion
+
+        #region Insert data
+
+        public void InsertIssue(int Type, string Title, int IssueStatus, int IssueSeverity, int RaisedBy, string Desc, int LeadFunction, string ImpactedFuncs, int Location,
+            string ImpactedVenues, string DateOccurence, string DateActualEnd, string DateUpdated, int Reportable, int Dashboard, string Creator)
+        {
+            SqlConnection conn = new SqlConnection(@connectionString);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+
+            string _sql = " INSERT INTO [dbo].[GIMissue] " +
+                          " ([Type]" +
+                          " ,[Title]" +
+                          " ,[IssueStatus]" +
+                          " ,[IssueSeverity]" +
+                          " ,[RaisedBy]" +
+                          " ,[Description]" +
+                          " ,[LeadFunction]" +
+                          " ,[ImpactedFunctions]" +
+                          " ,[Location]" +
+                          " ,[ImpactedVenues]" +
+                          " ,[DateOccurence]" +
+                          " ,[DateActualEnd]" +
+                          " ,[DateUpdated]" +
+                          " ,[Reportable]" +
+                          " ,[Dashboard]" +
+                          " ,[Creator])" +
+                          "  VALUES " +
+                          " (" + Type +
+                          " ," + Title +
+                          " ," + IssueStatus +
+                          " ," + IssueSeverity +
+                          " ," + RaisedBy +
+                          " ," + Desc +
+                          " ," + LeadFunction +
+                          " ," + ImpactedFuncs +
+                          " ," + Location +
+                          " ," + ImpactedVenues +
+                          " ," + DateOccurence +
+                          " ," + DateActualEnd +
+                          " ," + DateUpdated +
+                          " ," + Reportable +
+                          " ," + Dashboard +
+                          " ," + Creator + ")";
+
+            cmd.CommandText = _sql;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        public void InsertUpdate(int UpdatedBy, string Creator, string UpdateText, string UpdateType, string DateUpdate)
+        {
+            SqlConnection conn = new SqlConnection(@connectionString);
+            conn.Open();
+            SqlCommand cmd = conn.CreateCommand();
+
+            string _sql = "INSERT INTO [dbo].[GIMupdateLog] " +
+                          " ([UpdatedBy]" +
+                          " ,[Creator]" +
+                          " ,[UpdateText]" +
+                          " ,[UpdateType]" +
+                          " ,[DateUpdate])" +
+                          " VALUES" +
+                          " (" + UpdatedBy +
+                          " ," + Creator +
+                          " ," + UpdateText +
+                          " ," + UpdateType +
+                          " ," + DateUpdate + ")";
+
+            cmd.CommandText = _sql;
+            cmd.ExecuteNonQuery();
+            conn.Close();
+        }
+
+        #endregion
+
+        #region Update data
+
+        #endregion
+
+        #region Delete data
+
+        #endregion
+    }
+}
